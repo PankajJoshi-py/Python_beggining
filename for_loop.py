6265177ae7acab928e8d4d01483203902751db6d
PankajJoshi <pankajsharma143k@gmail.com>
2026-02-24 16:28:33 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,26 @@
+# print("meow\n"*3, end="")
+# while True:
+#     n = int(input("What is the main value?"))
+#     if n > 0:
+#         break
+# for _ in range (n):
+#     print("meow")
+# def main():
+#     number = get_number()
+#     meow(3)
+
+# def get_number():
+#     while True:
+#      n=int(input("Enter any integer"))
+#      if n > 0:
+#         return n
+    
+# def meow(n):
+#    for _ in range (n):
+#       print("meow")
+    
+# main()
+students = ["Hermione","Harry","Ron"]
+for i in range(len(students)):
+    print(i, students[i])
+ 
